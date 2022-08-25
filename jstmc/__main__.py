b626e720d66fa51ab696f8268ba931d70818e3b7
jochen.schmidt <jschmidt@cbs.mpg.de>
2022-08-25 14:10:29 +0200

    porting to newer pulseq version

--- jstmc/__main__.py
+++ jstmc/__main__.py
@@ -23,10 +23,10 @@ def main():
     path = Path("jstmc/images").absolute()
     # seq.ppSeq.plot()
 
-    # utils.pretty_plot_et(seq, t_start=seq.params.TR, save=path.joinpath("echo_train_central_semc.png"))
-    scan_time = np.sum(seq.ppSeq.arr_block_durations)
-    # utils.pretty_plot_et(seq, t_start=scan_time * 1e3 - 4*seq.params.TR, plot_blips=True,
-    # save=path.joinpath("echo_train_acc_tse.png"))
+    # utils.pretty_plot_et(seq, t_start=seq.params.TR)  # , save=path.joinpath("echo_train_central_semc.png"))
+    scan_time = np.sum(seq.ppSeq.block_durations)
+    # utils.pretty_plot_et(seq, t_start=scan_time * 1e3 - 4*seq.params.TR, plot_blips=True)
+    #  save=path.joinpath("echo_train_acc_tse.png"))
     logging.info(f"Total Scan Time: {scan_time / 60:.1f} min")
 
     # verifying
@@ -40,8 +40,9 @@ def main():
             w_file.write(f"{str(err_rep_item)}\n")
 
     seq.save()
-    logging.info(f".seq set definitions: {seq.ppSeq.dict_definitions}")
+    logging.info(f".seq set definitions: {seq.ppSeq.definitions}")
     seq.ppSeq.plot(time_range=(0, 2e-3 * seq.params.TR), time_disp='s')
+    seq.ppSeq.plot(time_range=(scan_time - 2e-3 * seq.params.TR, scan_time - 1e-6), time_disp='s')
 
 
 if __name__ == '__main__':
