fix slice freq offset sign

jstmc/options.py
@@ -92,6 +92,9 @@ class SequenceParameters(helpers.Serializable):
         self.deltaK_read = 1e3 / self.resolutionFovRead  # cast to m
         self.deltaK_phase = 1e3 / (self.resolutionFovRead * self.resolutionFovPhase / 100.0)  # cast to m
         self.TE = np.arange(1, self.ETL + 1) * self.ESP  # [ms] echo times
+        # there is one gap less than number of slices,
+        self.z_extend = self.resolutionSliceThickness * (
+                self.resolutionNumSlices + self.resolutionSliceGap / 100.0 * (self.resolutionNumSlices - 1))
         # acc
         self.numberOfOuterLines = round((self.resolutionNPhase - self.numberOfCentralLines) / self.accelerationFactor)
         # sequence
@@ -143,9 +146,9 @@ class SequenceParameters(helpers.Serializable):
         return self.resolutionVoxelSizeRead, self.resolutionVoxelSizePhase, self.resolutionSliceThickness
 
     def get_fov(self):
-        fov_read = 1e-3 * self.resolutionVoxelSizeRead * 64
-        fov_phase = 1e-3 * self.resolutionVoxelSizePhase * 64
-        fov_slice = self.resolutionSliceThickness * 1e-3 * self.resolutionNumSlices * (1 + self.resolutionSliceGap/100)
+        fov_read = 1e-3 * self.resolutionFovRead / 64
+        fov_phase = 1e-3 * self.resolutionFovPhase / 64
+        fov_slice = self.z_extend * 1e-3
         return fov_read, fov_phase, fov_slice
 
     def set_esp(self, esp: float):

jstmc/sequence.py
@@ -669,9 +669,8 @@ class SequenceBlockEvents:
     def _set_delta_slices(self):
         # multi-slice
         numSlices = self.seq.params.resolutionNumSlices
-        sliThick = self.seq.params.resolutionSliceThickness
-        # there is one gap less than number of slices, cast  thickness from mm / gap from %
-        delta_z = sliThick * (numSlices + self.seq.params.resolutionSliceGap / 100.0 * (numSlices - 1)) * 1e-3
+        # cast from mm
+        delta_z = self.seq.params.z_extend * 1e-3
         if self.seq.params.interleavedAcquisition:
             logModule.info("Set interleaved Acquisition")
             # want to go through the slices alternating from beginning and middle
@@ -697,7 +696,7 @@ class SequenceBlockEvents:
             grad_amplitude = self.refocusing.slice_grad.amplitude
             rf = self.refocusing.rf[pulse_num]
         # apply slice offset -> caution grad_amp in rad!
-        freq_offset = grad_amplitude * self.z[idx_slice]
+        freq_offset = - grad_amplitude * self.z[idx_slice]
         phase_offset = rf.init_phase - 2 * np.pi * freq_offset * pp.calc_rf_center(rf)[0]  # radiant again
         return freq_offset, phase_offset  # casting
         # freq to Hz, phase is in radiant here