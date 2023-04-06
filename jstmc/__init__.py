68a555e14f0bc2d169170e1d5ba3623b073cc85c
Jochen Schmidt <jschmidt@cbs.mpg.de>
2023-04-06 10:56:24 +0200

    fix source file import path settings

--- jstmc/__init__.py
+++ jstmc/__init__.py
@@ -1,4 +1,5 @@
-
 import sys
-sys.path.append("../pypulseq")
-from pypulseq import *
+import pathlib
+sources_root_path = pathlib.Path(__file__).absolute().parent.parent
+sys.path.append(sources_root_path.joinpath("pypulseq").as_posix())
+sys.path.append(sources_root_path.joinpath("rf_pulse_files").as_posix())
